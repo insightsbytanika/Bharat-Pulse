840d9bb99d05b1372ecad6701952809d87ea4308
Tanika Gupta <insightsbytanika@gmail.com>
2026-06-14 23:23:06 +0530

    changes done

--- model.py
+++ model.py
@@ -29,3 +29,5 @@ def train_model(df):
     X= df["clean_text"] 
     y= df["sentiment"]
     X_train , X_test , y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+    vectorizer=TfidfVectorizer(max_features=5000)
+    X_train_vec=vectorizer.fit.transform(X_test)
