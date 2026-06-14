805f469a57b555eb432cda8c6c881a3afffe6c6d
Tanika Gupta <insightsbytanika@gmail.com>
2026-06-14 23:27:49 +0530

    changes made

--- model.py
+++ model.py
@@ -31,3 +31,4 @@ def train_model(df):
     X_train , X_test , y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
     vectorizer=TfidfVectorizer(max_features=5000)
     X_train_vec=vectorizer.fit_transform(X_train)
+    X_text_vec=vectorizer.transform(X_test)
